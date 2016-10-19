9fd8df6c8ea260d2b9bd8338af6a51f0f128f8f3
jdr81 <jdr81@DESKTOP-38VD2HD>
2016-10-19 13:36:00 -0500

    Commited for demo

---
+++ WCFVerySecure/ICustomerService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using WcfDemo;
+
+namespace WCFVerySecure
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ICustomerService
+
+    {
+
+        [OperationContract]
+
+        List<Customer> GetCustomerList();
+
+    }
+}
