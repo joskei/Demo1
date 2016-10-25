3cc9e5e31647daf4881e328760db7a490295d6ca
jdr81 <jdr81@DESKTOP-38VD2HD>
2016-10-25 16:02:34 -0500

    added for demo2

---
+++ BoxUnbox/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace BoxUnbox
+{
+    public class Class1
+    {
+        public void TestBoxUnbox()
+        {
+            var i = 2;
+            object o = i;
+
+            var z = (int)o;
+        }
+        
+        
+    }
+}
---
+++ CalculatorLib/MyMath.vb
@@ -0,0 +1,10 @@
+﻿Public Class MyMath
+    Public Function Add(val1 As Integer, val2 As Integer) As Integer
+        Return val1 + val2
+    End Function
+
+    Public Function Subtract(val1 As Integer, val2 As Integer) As Integer
+        Return val1 - val2
+    End Function
+
+End Class
