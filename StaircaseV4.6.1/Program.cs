f279db9da54e8374f3ba25343c1523071658f1fd
jdr81 <jdr81@DESKTOP-38VD2HD>
2016-10-26 14:14:35 -0500

    Added some solns

---
+++ StaircaseV4.6.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaircaseV4._6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StairCase(4);
+            Console.ReadLine();
+        }
+
+        public static void StairCase(int n)
+        {
+            for (var i = 1; i <= n; i++)
+            {                
+                Console.WriteLine(new string(' ', n - i) + new string('#', i));
+            }
+        }
+    }
+}
---
+++ TestBoxingUnboxing1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBoxingUnboxing1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repeatTime = 1000000000;
+
+            var watch0 = System.Diagnostics.Stopwatch.StartNew();
+
+            for (var i = 0; i < repeatTime; i++)
+            {
+                BoxingTest();                
+            }
+
+            watch0.Stop();
+            Console.WriteLine("Time taken with Boxing: {0}ms", watch0.Elapsed.TotalMilliseconds);
+
+            var watch1 = System.Diagnostics.Stopwatch.StartNew();
+
+            for (var i = 0; i < repeatTime; i++)
+            {
+                UnBoxingTest();
+            }
+
+            watch1.Stop();
+            Console.WriteLine("Time taken with UnBoxing: {0}ms", watch1.Elapsed.TotalMilliseconds);
+
+
+            var watch2 = System.Diagnostics.Stopwatch.StartNew();
+
+            for (var i = 0; i < repeatTime; i++)
+            {
+                NoBoxUnbox();
+            }
+
+            watch2.Stop();
+            Console.WriteLine("Time taken without Boxing or unboxing: {0}ms", watch2.Elapsed.TotalMilliseconds);
+
+
+            Console.ReadLine();
+        }
+
+        private static void BoxingTest()
+        {
+            const int z = 312;
+
+            object j = z;
+        }
+
+        private static void UnBoxingTest()
+        {
+            object j = 555;
+            var z = j;
+        }
+
+        private static void NoBoxUnbox()
+        {
+            const int z = 312;
+            var y = z;
+        }
+    }
+}
