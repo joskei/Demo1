bdc34d730623313c0efe92743a45b07df0173ac1
jdr81 <jdr81@DESKTOP-38VD2HD>
2016-10-25 22:54:51 -0500

    ForDemo

---
+++ LonelyInteger/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace LonelyInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an array of number separated by spaces: ");
+            var input = Console.ReadLine();
+            if (input == null)
+            {
+                Console.WriteLine("Error! User did not enter a value.");
+                Thread.Sleep(2000);
+                return;
+            }
+            var ar = input.Split(' ');
+
+            var res = 0;
+            for (var i = 0; i < ar.Length; i++) /* Variable size has the value of the size of the array*/
+            {
+                res = res ^ int.Parse(ar[i]);
+            }
+
+            Console.WriteLine("Lonely Integer is : " + res);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Staircase/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StairCase(4);
+            Console.ReadLine();
+        }
+
+        public static void StairCase(int n)
+        {
+            for (var i = 1; i <= n; i++)
+            {
+                var a = new string(' ', n - i) + new string('#', i);
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
