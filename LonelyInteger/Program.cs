c89be23bcf59864945e96480b863a1b35b5f09a0
jdr81 <jdr81@DESKTOP-38VD2HD>
2016-10-28 11:39:52 -0500

    Modified lonelyplanet to be oneliner

--- LonelyInteger/Program.cs
+++ LonelyInteger/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
@@ -9,6 +10,14 @@ namespace LonelyInteger
     {
         static void Main(string[] args)
         {
+            /*
+             Consider an array of  integers, A = [a0,a1,a2,…,an-1] , where all but one of the integers occur in pairs. 
+             In other words, every element in A occurs exactly twice except for one unique element.
+
+                Sample Input: 4 5 6 5 6 3 4
+                Sample Output: 3
+
+             */
             Console.Write("Please enter an array of number separated by spaces: ");
             var input = Console.ReadLine();
             if (input == null)
@@ -20,11 +29,23 @@ namespace LonelyInteger
             var ar = input.Split(' ');
 
             var res = 0;
-            foreach (var item in ar)
+
+            var tmpAr = new List<int>();
+            //Convert to List<int>
+            foreach (var item  in ar)
             {
-                res = res ^ int.Parse(item);
+                tmpAr.Add(int.Parse(item));
             }
-            
+
+            //Code Here
+
+            //foreach (var item in ar)
+            //{
+            //    res = res ^ int.Parse(item);
+            //}
+
+            tmpAr.ForEach(a => res = res ^ a);
+
             Console.WriteLine("Lonely Integer is : " + res);
             Console.ReadLine();
 
